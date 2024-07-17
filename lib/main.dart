7a8a26f7a3e0e99de227e96ebe496dbb5275ee04
Datbt <buithanhdat15051997@gmail.com>
2024-07-17 13:54:37 +0700

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,36 @@
-import 'package:demo/quiz.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Quiz());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Demo Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Home page'),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: const Center(
+          child:
+          Text(
+            'Hello World',
+          )
+      ),
+    );
+  }
 }
