6d02f00bd3c7b258a7f8204ca9d71ba948da2ff2
Datbt <buithanhdat15051997@gmail.com>
2024-07-29 16:23:51 +0700

    The first lesson using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(ChangeNotifierProvider(
+      create: (_) => CounterProvider(),
+      child: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -15,6 +20,18 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class CounterProvider extends ChangeNotifier {
+  int _counter = 100;
+
+  int get counter => _counter;
+
+  void add() {
+    _counter++;
+    notifyListeners();
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -25,11 +42,15 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(this.title),
       ),
-      body: const Center(
-          child:
-          Text(
-            'Hello World',
-          )
+      body: Center(
+          child: Text(
+        context.watch<CounterProvider>().counter.toString(),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          context.read<CounterProvider>().add();
+        },
       ),
     );
   }
