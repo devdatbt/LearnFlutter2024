b041ff9778ccabad1c03fd25a2cbd3e7ee0a4da2
Datbt <buithanhdat15051997@gmail.com>
2024-07-29 17:12:47 +0700

    The second lesson using provider with consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,83 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(ChangeNotifierProvider(
-      create: (_) => CounterProvider(),
-      child: MyApp(),
+      create: (_) => MySetting(),
+      child: MaterialApp(
+        home: MyApp(),
+      ),
     ));
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World Demo Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Home page'),
-    );
+    return Consumer<MySetting>(builder: (context, mySettings, child) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mySettings.color,
+          title: Text("Provider demo"),
+        ),
+        drawer: Drawer(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      mySettings.changeText();
+                      Navigator.pop(context);
+                    },
+                    child: Text('Change text')),
+                ElevatedButton(
+                    onPressed: () {
+                      mySettings.changeColor();
+                      Navigator.pop(context);
+                    },
+                    child: Text('Change color')),
+              ],
+            ),
+          ),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    mySettings.changeText();
+                  },
+                  child: Text('Change text')),
+              Text(mySettings.text)
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
 
-class CounterProvider extends ChangeNotifier {
-  int _counter = 100;
+class MySetting extends ChangeNotifier {
+  String text = 'Done';
+  Color color = Colors.red;
 
-  int get counter => _counter;
-
-  void add() {
-    _counter++;
+  void changeText() {
+    if (text == 'Hello') {
+      text = 'World';
+    } else {
+      text = 'Hello';
+    }
     notifyListeners();
   }
-}
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),
-      ),
-      body: Center(
-          child: Text(
-        context.watch<CounterProvider>().counter.toString(),
-      )),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          context.read<CounterProvider>().add();
-        },
-      ),
-    );
+  void changeColor() {
+    if (color == Colors.red) {
+      color = Colors.green;
+    } else {
+      color = Colors.red;
+    }
+    notifyListeners();
   }
 }
